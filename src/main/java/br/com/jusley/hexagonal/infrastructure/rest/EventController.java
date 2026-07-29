8f3d94e36a25a6c45fbad97257ce73c39bacfaf4
Jusley Souza <jusleysouza31@gmail.com>
2026-07-28 21:29:15 -0300

    feat(rest): Adding Event REST endpoints

---
+++ src/main/java/br/com/jusley/hexagonal/infrastructure/rest/EventController.java
@@ -0,0 +1,53 @@
+package br.com.jusley.hexagonal.infrastructure.rest;
+
+import br.com.jusley.hexagonal.application.exceptions.ValidationException;
+import br.com.jusley.hexagonal.application.usecases.event.CreateEventUseCase;
+import br.com.jusley.hexagonal.application.usecases.event.SubscribeCustomerToEventUseCase;
+import br.com.jusley.hexagonal.infrastructure.dtos.NewEventDTO;
+import br.com.jusley.hexagonal.infrastructure.dtos.SubscribeDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import java.net.URI;
+import java.util.Objects;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+//Adapter para a rest api
+@RestController
+@RequestMapping(value = "events")
+public class EventController {
+
+    private final CreateEventUseCase createEventUseCase;
+
+    private final SubscribeCustomerToEventUseCase  subscribeCustomerToEventUseCase;
+
+    public EventController(
+            final CreateEventUseCase createEventUseCase,
+            final SubscribeCustomerToEventUseCase subscribeCustomerToEventUseCase) {
+        this.createEventUseCase = Objects.requireNonNull(createEventUseCase);
+        this.subscribeCustomerToEventUseCase = Objects.requireNonNull(subscribeCustomerToEventUseCase);
+    }
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public ResponseEntity<?> create(@RequestBody NewEventDTO dto) {
+        try {
+            final var output = createEventUseCase.execute(new CreateEventUseCase.Input(dto.name(), dto.date(), dto.totalSpots(), dto.partnerId()));
+            return ResponseEntity.created(URI.create("/events/" + output.id())).body(output);
+        } catch (ValidationException ex) {
+            return ResponseEntity.unprocessableEntity().body(ex.getMessage());
+        }
+    }
+
+    @Transactional
+    @PostMapping(value = "/{id}/subscribe")
+    public ResponseEntity<?> subscribe(@PathVariable String id, @RequestBody SubscribeDTO dto) {
+        try {
+            final var output = subscribeCustomerToEventUseCase.execute(new SubscribeCustomerToEventUseCase.Input(id, dto.customerId()));
+            return ResponseEntity.ok(output);
+        } catch (ValidationException ex) {
+            return ResponseEntity.unprocessableEntity().body(ex.getMessage());
+        }
+    }
+}
